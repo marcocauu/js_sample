947a4be17a919006b4adaa876f2acebfafc4aec4
marcocauu <marco.auu@live.com>
2019-09-01 21:15:41 -0700

    initial commit v2

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # js_sample
 Collection of Javascript related work.
+
+To run the project:
+```
+npm i
+```
+
+Testing the project:
+```
+npm test
+```
--- src/defang.js
+++ src/defang.js
@@ -1,3 +1,9 @@
+/**
+ * 
+ * Leetcode question pulled from https://leetcode.com/problems/defanging-an-ip-address/
+ * @param {*} ipAddress string
+ * @return string
+ */
 const defang = (ipAddress = '') => {
   if (!ipAddress) {
     return '';
